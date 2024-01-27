98f649e8692bbd739ce1e68ceae174a8ae507c30
sergei alekseev <alekseev@lifehacker.ru>
2024-01-27 13:13:49 +0300

    добавлены вопросы vue

---
+++ src/questions/vue.ts
@@ -0,0 +1,126 @@
+import { ask } from "@/types/general"
+
+const vueAsk: Array<ask> = [
+    {
+        id: 1,
+        theme: 'vue',
+        ask: 'Что такое vue?',
+        level: 1
+    },
+    {
+        id: 2,
+        theme: 'vue',
+        ask: 'Назовите хуки жизненного цикла vue2/vue3/nuxt',
+        level: 2
+    },
+    {
+        id: 3,
+        theme: 'vue',
+        ask: 'Что нужно сделать, чтобы использовать v-model на компонентах?',
+        level: 2
+    },
+    {
+        id: 4,
+        theme: 'vue',
+        ask: 'Чем поведение пропса с типом Boolean отличается от других?',
+        level: 2
+    },
+    {
+        id: 5,
+        theme: 'vue',
+        ask: 'Что вы знаете о функциональных компонентах?',
+        level: 2
+    },
+    {
+        id: 6,
+        theme: 'vue',
+        ask: 'Как отловить ошибки в дочерних компонентах?',
+        level: 2
+    },
+    {
+        id: 7,
+        theme: 'vue',
+        ask: 'Что такое slot-scope?',
+        level: 2
+    },
+    {
+        id: 8,
+        theme: 'vue',
+        ask: 'Чем отличается работа vue-router с режим history от стандартного?',
+        level: 2
+    },
+    {
+        id: 9,
+        theme: 'vue',
+        ask: 'Назовите хуки vue-router, которые доступны в компоненте',
+        level: 2
+    },
+    {
+        id: 10,
+        theme: 'vue',
+        ask: 'Отличия миксинов от подхода модулей в composition api',
+        level: 1
+    },
+    {
+        id: 11,
+        theme: 'vue',
+        ask: 'Расскажите про computed, в каких случаях вычисляемое свойство будет заново пересчитано?',
+        level: 1
+    },
+    {
+        id: 12,
+        theme: 'vue',
+        ask: 'Что такое модификаторы',
+        level: 2
+    },
+    {
+        id: 13,
+        theme: 'vue',
+        ask: 'Что будет, если опции нашего компонента и миксина будут одинаковыми?',
+        level: 2
+    },
+    {
+        id: 14,
+        theme: 'vue',
+        ask: 'Как реализована реактивность во vue2/vue3?',
+        level: 2
+    },
+    {
+        id: 15,
+        theme: 'vue',
+        ask: 'Что нового во vue3?',
+        level: 2
+    },
+    {
+        id: 16,
+        theme: 'vue',
+        ask: 'Что такое прогрессивный рендеринг?',
+        level: 2
+    },
+    {
+        id: 17,
+        theme: 'vue',
+        ask: 'Какие архитектурные-паттерны проектирования веб-приложений вы знаете? какой используе vue?',
+        level: 2
+    },
+    {
+        id: 18,
+        theme: 'vue',
+        ask: 'Что делает метод nextTick?',
+        level: 2
+    },
+    {
+        id: 19,
+        theme: 'vue',
+        ask: 'Что такое virtual DOM?',
+        level: 1
+    },
+    {
+        id: 20,
+        theme: 'vue',
+        ask: 'Какие есть режим в Nuxt?',
+        level: 2
+    },
+]
+
+export default vueAsk
